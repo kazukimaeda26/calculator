06d68495d5be6dbeff985c7ea27c82c42e956425
kazukimaeda26 <kazukimaeda26@gmail.com>
2021-06-08 00:33:23 +0900

    viewの作成

---
+++ src/components/Button.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const Button = ({text}) => {
+  return (
+    <button>{text}</button>
+  )
+}
+export default Button;
---
+++ src/components/Result.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Result = ({result}) => {
+  return (
+    <div className="resultValue">
+      {result}
+    </div>
+  )
+}
+export default Result;
